ee72388db1cf3700390860b4738b2606ef8e98e6
Northcoders15 <northcoders15@Northcoders15s-MacBook-Pro.local>
2019-03-25 11:18:59 +0000

    moved location of files

---
+++ build/precache-manifest.ef352c849ae6f2ecb4e20dd3dd2f80cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c46d8d8df143dcbdcba3",
+    "url": "/static/css/main.4418c1b2.chunk.css"
+  },
+  {
+    "revision": "c46d8d8df143dcbdcba3",
+    "url": "/static/js/main.9d36fede.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "94a0ab6dddc256240808",
+    "url": "/static/js/2.e5ff04e0.chunk.js"
+  },
+  {
+    "revision": "3fa19bd10d402efc659afa029b05c306",
+    "url": "/index.html"
+  }
+];
